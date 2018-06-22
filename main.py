f9fa3b93e24cb43d74b87b884dc04f15a703db01
DentalDirectoryDemo <40489826+DentalDirectoryDemo@users.noreply.github.com>
2018-06-22 13:20:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!, Hows lIfe Today'
+  return 'Hello, World!, Hows life Today'
 
 if __name__ == '__main__':
   app.run()
